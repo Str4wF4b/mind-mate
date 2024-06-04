bab380885c8f28794142402abe930a9bb220fffe
Str4wF4b <fabioalta25@gmail.com>
2024-06-04 20:36:03 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Mind Mate',
+  webDir: 'www'
+};
+
+export default config;
