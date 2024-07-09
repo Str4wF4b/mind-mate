4695db21ed454e972afc067880d7297a22b36d1a
Fabio <fabioalta25@gmail.com>
2024-07-09 19:46:28 +0200

    loading screen

---
+++ src/app/pages/loader/loader.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { LoaderPageRoutingModule } from './loader-routing.module';
+
+import { LoaderPage } from './loader.page';
+import { TabsPageModule } from 'src/app/tabs/tabs.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    LoaderPageRoutingModule,
+    TabsPageModule
+  ],
+  declarations: [LoaderPage]
+})
+export class LoaderPageModule {}
